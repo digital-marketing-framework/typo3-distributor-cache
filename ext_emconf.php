acfa16977a401ea48d4c97815fbaaf252e985138
Stephan Ude <ude@mediatis.de>
2023-06-17 16:47:07 +0200

    [TASK] typo3 12 requirements

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,17 +1,16 @@
 <?php
 
 $EM_CONF[$_EXTKEY] = [
-    'title' => 'Digital Marketing Framwork - Distributor - Cache',
+    'title' => 'Digital Marketing Framework - Distributor - Cache',
     'description' => 'Update collector cache with newly distributed data',
     'category' => 'be',
-    'author' => 'Michael Vöhringer',
-    'author_email' => 'voehringer@mediatis.de',
+    'author_email' => 'info@mediatis.de',
     'author_company' => 'Mediatis AG',
     'state' => 'stable',
     'version' => '1.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.0-11.5.99',
+            'typo3' => '11.5.0-12.4.99',
         ],
         'conflicts' => [
         ],
