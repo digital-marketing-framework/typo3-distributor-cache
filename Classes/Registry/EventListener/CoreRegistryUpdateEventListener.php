f4e3d128828b785464b31a831c85455b9830f44f
Stephan Ude <ude@mediatis.de>
2023-08-09 19:49:53 +0200

    [TASK] configuration document version handling

---
+++ Classes/Registry/EventListener/CoreRegistryUpdateEventListener.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace DigitalMarketingFramework\Typo3\Distributor\Cache\Registry\EventListener;
+
+use DigitalMarketingFramework\Distributor\Cache\DistributorCacheInitialization;
+use DigitalMarketingFramework\Typo3\Core\Registry\EventListener\AbstractCoreRegistryUpdateEventListener;
+
+class CoreRegistryUpdateEventListener extends AbstractCoreRegistryUpdateEventListener
+{
+    public function __construct()
+    {
+        parent::__construct(new DistributorCacheInitialization());
+    }
+}
